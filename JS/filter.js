c50aa59a66e92d36b7a624cf1968740b7d638cdb
nitipat21 <nitipat.rstw@gmail.com>
2022-03-15 19:49:46 +0700

    updated name and price filter

--- JS/filter.js
+++ JS/filter.js
@@ -1,4 +1,5 @@
 const   filterList = document.querySelector(".filter-list"),
+        filterSearch = document.querySelector(".filter-search"),
         btnFilterAll = document.querySelector(".filter-list-1"),
         btnFilterCompanyA = document.querySelector(".filter-list-2"),
         btnFilterCompanyB = document.querySelector(".filter-list-3"),
@@ -6,7 +7,8 @@ const   filterList = document.querySelector(".filter-list"),
         btnFilterCompanyD = document.querySelector(".filter-list-5"),
         volumeFilter = document.querySelector(".filter-price-volume"),
         volumeAmount = document.querySelector(".filter-price-volume-amount"),
-        btnVolumeFilter = volumeFilter.firstElementChild;
+        btnVolumeFilter = volumeFilter.firstElementChild,
+        btnClearFilter = document.querySelector(".filter-clear-btn");
 // 
 btnFilterAll.addEventListener("click",filterCompanyName.bind(this,btnFilterAll));
 btnFilterCompanyA.addEventListener("click",filterCompanyName.bind(this,btnFilterCompanyA));
@@ -14,52 +16,69 @@ btnFilterCompanyB.addEventListener("click",filterCompanyName.bind(this,btnFilter
 btnFilterCompanyC.addEventListener("click",filterCompanyName.bind(this,btnFilterCompanyC));
 btnFilterCompanyD.addEventListener("click",filterCompanyName.bind(this,btnFilterCompanyD));
 btnVolumeFilter.addEventListener("change",filterPriceRange.bind(this,volumeAmount));
+btnClearFilter.addEventListener("click",clearFilter.bind(this));
+filterSearch.addEventListener("keypress",matchLetters);
 // 
-function filterCompanyName (name) {
+function filterCompanyName (name){
     const   filterName = name.textContent,
             productListChild = productList.children,
             productListChildArray = Array.from(productListChild);
+
+            selectPriceFilter(btnVolumeFilter.value);
             filterPriceRange(btnVolumeFilter.value);
             selectNameFilter(name);
-            
+
             productListChildArray.forEach(function(article){
-                if (!article.classList.contains("hide-product")) {
+                if (!article.classList.contains("hide-product-price")) {
                     if (filterName === "All") {
-                        article.classList.remove("hide-product");
+                        article.classList.remove("hide-product-name");
                     } else {
                         if (!article.classList.contains(filterName)) {
-                            article.classList.add("hide-product");
+                            article.classList.add("hide-product-name");
                         } else {
-                            article.classList.remove("hide-product");
+                            article.classList.remove("hide-product-name");
                         }
                     }
                 }
             });
 }
 
-function filterPriceRange (price) {
-    const   priceRange = activePriceFilter(price),
+function selectNameFilter (name){
+    const filterListArray = Array.from(filterList.children);
+
+    filterListArray.forEach(function(value){
+        if (name !== value.textContent)
+            if (value.classList.contains("selected")) {
+                value.classList.remove("selected");
+            }
+    })
+
+    name.classList.add("selected");
+}
+
+function filterPriceRange (price){
+    const   priceRange = selectPriceFilter(price),
             priceRangNum = parseFloat(priceRange.split("$")[1].replace(/,/g, "")),
             productListChild = productList.children,
             productListChildArray = Array.from(productListChild);
-            
+
             productListChildArray.forEach(function(article){
                 const   productPrice = article.lastElementChild.lastElementChild.textContent,
                         productPriceNum = parseFloat(productPrice.replace(/,/g, ""));
 
                 if (priceRangNum === "0") {
-                    article.classList.remove("hide-product");
+                    article.classList.remove("hide-product-price");
                 } else {
                     if (priceRangNum > productPriceNum) {
-                        article.classList.add("hide-product");
+                        article.classList.add("hide-product-price");
                     } else if (priceRangNum <= productPriceNum) {
-                        article.classList.remove("hide-product");
+                        article.classList.remove("hide-product-price");
                     }
                 }
             })
 }
 
-function activePriceFilter (price){
+function selectPriceFilter (price){
     const formatter = new Intl.NumberFormat('en-US', {
         style: 'currency',
         currency: 'USD',
@@ -72,15 +91,24 @@ function activePriceFilter (price){
     return priceRange;
 }
 
-function selectNameFilter (name) {
-    const filterListArray = Array.from(filterList.children);
+function clearFilter (){
+    filterSearch.value = "";
+    btnVolumeFilter.value = 0;
+    volumeAmount.textContent = `$${0}-5,000`;
+    filterCompanyName(btnFilterAll);
+}
 
-    filterListArray.forEach(function(value){
-        if (name !== value.textContent)
-            if (value.classList.contains("selected")) {
-                value.classList.remove("selected");
-            }
-    })
+function getProductName(key){
+    const   storeLocalStorage = getLocalStorage(key),
+            storeArray = Array.from(storeLocalStorage),
+            productNameArray = [];
 
-    name.classList.add("selected");
+    storeArray.forEach(product => productNameArray.push(product.name));
+    
+    return productNameArray;
+}
+
+function matchLetters(){
+    const   allProductName = getProductName("store");
+    console.log(allProductName);
 }
